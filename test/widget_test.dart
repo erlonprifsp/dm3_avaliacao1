dd8759d630c97321dcde060652ee1b11cd3efea9
erlonprifsp <erlon.rodrigues@aluno.ifsp.edu.br>
2024-09-16 13:42:19 -0300

    fix(widget_test.dart): corrige erro no arquivo de teste

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,6 +13,7 @@ import 'package:dm3_avaliacao1/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
+    // await tester.pumpWidget(const MyApp());
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
